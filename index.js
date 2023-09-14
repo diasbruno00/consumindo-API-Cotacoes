eb874a83cea132f9b15c6edd6f78f27200c9f425
bruno dias <diasbruno00@gmail.com>
2023-09-14 08:14:23 -0300

    adicionado tratamento de erro

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 const getDados = async () => {
-  const response = await axios.get(
-    `https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL`
-  );
-  return response.data;
+  try {
+    const response = await axios.get(
+      `https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Erro ao consumir dados API`)
+    return
+  }
 };
 
 const converter = async (codigo, valor) => {
-
   const cotacaoDados = await getDados();
 
   let resultado = {
@@ -30,31 +34,29 @@ const converter = async (codigo, valor) => {
   return resultado;
 };
 
-
 const start = () => {
+  const date = new Date();
 
   setInterval(async () => {
     const arrayCodigos = ["USD", "EUR", "BTC"];
     const escolhaAleatoria = parseInt(Math.random() * arrayCodigos.length);
     const valor = Math.random() * 100;
     const resultado = await converter(arrayCodigos[escolhaAleatoria], valor);
-  
+
     console.log(
       `Moeada Brasileira: R$ ${valor.toFixed(2)}
-      Conversão para ${
-        arrayCodigos[escolhaAleatoria]
-      }: ${resultado.conversao}
-      Cotação ${arrayCodigos[escolhaAleatoria]} :  ${resultado.cotacao} \n`
+      Conversão para ${arrayCodigos[escolhaAleatoria]}: ${resultado.conversao}
+      Cotação ${arrayCodigos[escolhaAleatoria]} :  ${resultado.cotacao} 
+      Data atual: ${date.toLocaleDateString()}\n`
     );
   }, minutos);
-  
-}
+};
 
 const converterMoeda = (valor, cotacao) => {
   const valorAtualizado = valor / cotacao;
   return valorAtualizado;
 };
 
-const minutos = (60 * 2) * 1000; // 2 minutos
+const minutos = 60 * 2 * 1000; // 2 minutos
 
-start()
+start();
